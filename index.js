18a2a87cac5cb04457481485f632ad84b9c392c6
Herve176 <hdjomo1@gmail.com>
2024-03-03 13:47:40 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+let number = document.getElementById("number");
+let img = document.getElementById("image");
+let btn = document.getElementById("btn");
+
+
+btn.addEventListener("click", function () {
+   let i = Math.floor(Math.random() * 7);
+   switch(i)
+   {
+      case 1:
+         img.innerHTML.src =  './src/'+ 1+'.png';
+         break;
+         case 2:
+            img.innerHTML.src = './src/' + 2 + '.png';
+         break;
+         case 3:
+            img.innerHTML.src = './src/' + 3 + '.png';
+         break;
+         case 4:
+            img.innerHTML.src = './src/' + 4 + '.png';
+         break;
+         case 5:
+            img.innerHTML.src = './src/' + 5 + '.png';
+         break;
+         case 6:
+            img.innerHTML.src = './src/' + 6 + '.png';
+         break;
+
+   }
+   if (number != i) {
+      alert("you lose mother fucker.replay press ok!")
+   }
+});
